e5d591f400c466693a61f882973ba7ced997cac1
Alexis Bernier <alexiisbernier01@gmail.com>
2026-06-27 22:41:00 -0400

    Implemented TexturePlugin.kt and registered MainActivity.kt

---
+++ flutter_ui/android/app/src/main/kotlin/com/example/camera_mvp/TexturePlugin.kt
@@ -0,0 +1,66 @@
+package com.example.camera_mvp
+
+import android.view.Surface
+import androidx.annotation.Keep
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.view.TextureRegistry
+
+@Keep
+class TexturePlugin : FlutterPlugin {
+    private var channel: MethodChannel? = null
+    private var producer: TextureRegistry.SurfaceProducer? = null
+    private var nativeHandle: Long = 0
+
+    private external fun nativeAttach(surface: Surface, width: Int, height: Int): Long
+    private external fun nativeDetach(handle: Long)
+
+    companion object {
+        init {
+            System.loadLibrary("StrokeCVLib")
+        }
+    }
+
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        val targetWidth = 1920
+        val targetHeight = 1080
+
+        val prod = binding.textureRegistry.createSurfaceProducer()
+        prod.setSize(targetWidth,targetHeight)
+        producer = prod
+
+        prod.setCallback(object: TextureRegistry.SurfaceProducer.Callback {
+            override fun onSurfaceAvailable() {
+                if (nativeHandle == 0L) {
+                    nativeHandle = nativeAttach(prod.surface, targetWidth, targetHeight)
+                }
+            }
+            override fun onSurfaceCleanup() {
+                if (nativeHandle != 0L) {
+                    nativeDetach(nativeHandle)
+                    nativeHandle = 0L
+                }
+            }
+        })
+
+        channel = MethodChannel(binding.binaryMessenger, "texture-backend").apply {
+            setMethodCallHandler { call, result ->
+                if (call.method == "textureid") {
+                    result.success(prod.id())
+                } else {
+                    result.notImplemented()
+                }
+            }
+        }
+    }
+
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        if (nativeHandle != 0L) {
+            nativeDetach(nativeHandle)
+            nativeHandle = 0L
+        }
+        channel?.setMethodCallHandler(null)
+        channel =  null
+        producer = null
+    }
+}
--- flutter_ui/lib/main.dart
+++ flutter_ui/lib/main.dart
@@ -1,8 +1,27 @@
 import 'dart:ffi' as ffi;
 import 'dart:io';
 
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 import 'camera_bindings.dart' as bindings;
+
+// Fundementally, you need to include a file that contains a function
+// that will allow you to invoke a method from Java called
+// TextureRegistry.SurfaceProducer, in order for you to obtain the textureID
+// for the backend texture the Texture widget refers to.  This method will
+// create a SurfaceTexture which is the consumer of an
+// image buffer. The Surface object (from the SurfaceTexture: by invoking
+// .getSurface()), once registered and "bound" to the Texture widget can be
+// passed as an argument (in C++) to ANativeWindow_fromSurface().
+
+// ANativeWindow_fromSurface() generates a raw pointer to an object called
+// ANativeWindow. ANativeWindow is required to connect the raw pixel date
+// produced by the camera to the OpenGL External Texture. At a fundemental
+// level, ANativeWindow will serve as the connection between the Texture widget
+// and the OpenGL External Texture. All of the steps explained above are
+// required to initialize it.
 
 void main() {
   runApp(const MainApp());
