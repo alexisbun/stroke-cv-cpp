fdd5a7a20cc7a624440184c7359522ff0a08cb0d
Alexis Bernier <alexiisbernier01@gmail.com>
2026-07-02 15:04:14 -0400

    jni_native.h forward declarations

--- cpp_lib/include/jni_native.h
+++ cpp_lib/include/jni_native.h
@@ -1,8 +1,8 @@
 // Here, this approach uses static resolution (name mangling) for my JNI-C+ bridge. 
 // In the future, I may transition to dynamic registration if I were to encorperate more JNI funcitons.
-
 #include <jni.h>
 #include <android/native_window.h>
+#include <android/native_window_jni.h>
 
 #ifdef __cplusplus
 extern "C"
@@ -20,10 +20,15 @@ Java_com_example_camera_1mvp_TexturePlugin_nativeAttach(
 );
 
 // See TexturePlugin.kt for external Kotlin declaration of nativeDetach. 
-JNIEXPORT jlong JNICALL
+JNIEXPORT void JNICALL
 Java_com_example_camera_1mvp_TexturePlugin_nativeDetach(
     JNIEnv* env,
     jobject thiz,
     jlong handle
 );
+
+ANativeWindow* ANativeWindow_fromSurface(JNIEnv* env, jobject surface);
+void ANativeWindow_release(ANativeWindow* window);
+int32_t ANativeWindow_setBuffersGeometry(ANativeWindow* window, int32_t width, int32_t height, int32_t format); // Use hardware_buffer.h for 'format' param 
+// (https://android.googlesource.com/platform/frameworks/native/+/master/libs/nativewindow/include/android/hardware_buffer.h).
 }
