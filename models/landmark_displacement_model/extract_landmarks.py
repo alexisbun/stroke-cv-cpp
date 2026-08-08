c403254ac536f9635fcd40d753b5cd1112923a39
Alexis Bernier <alexiisbernier01@gmail.com>
2026-08-07 20:05:54 -0400

    implemented pytorch geometric graph serialized as .pt

--- models/landmark_displacement_model/extract_landmarks.py
+++ models/landmark_displacement_model/extract_landmarks.py
@@ -10,6 +10,9 @@ from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 import cv2
 from pathlib import Path
+from mediapipe.tasks.python.vision import FaceLandmarksConnections
+import torch
+from torch_geometric.data import Data, InMemoryDataset
 
 # Initialize mediapipe face mesh model
 def init_mp():
@@ -41,9 +44,8 @@ def extract_landmarks(img_path, face_mesh):
 
 def generate_dataset_npz(synthetic_dataset_path, output_npz_path):
     # Load the image files in /home/alexis/Desktop/synthetic-dataset/ComfyUI/output/cfd_target/
-        # Odd numbers (starting at result_00001) are normal faces
-        # Even numbers (starting at result_00002) altered  faces, same person as normal
-    
+    # Odd numbers (starting at result_00001) are normal faces
+    # Even numbers (starting at result_00002) altered  faces, same person as normal
     face_mesh = init_mp()
     normal = []
     altered = []
@@ -51,26 +53,26 @@ def generate_dataset_npz(synthetic_dataset_path, output_npz_path):
     num_undetected_faces = 0
 
     print(f"Extracting landmarks from {synthetic_dataset_path}")
-    for image_path in dir_path.iterdir():
-        match = re.search(r'\d+', image_path.name)
-        if not match:
+    max_person_id = max([int(re.search(r'\d+', f.name).group(0)) for f in dir_path.glob('*result_*')]) // 2
+    for person_id in range(1, max_person_id + 1):
+        normal_idx = 2 * person_id - 1 
+        altered_idx = 2 * person_id 
+
+        normal_img = list(dir_path.glob(f"*result_{normal_idx:05d}_*"))
+        altered_img = list(dir_path.glob(f"*result_{altered_idx:05d}_*"))
+
+        if not normal_img or not altered_img:
             continue
 
-        file_index = int(match.group(0))
+        norm_coords = extract_landmarks(normal_img[0], face_mesh)
+        alt_coords = extract_landmarks(altered_img[0], face_mesh)
 
-        try:
-            landmark_coords = extract_landmarks(image_path, face_mesh)
-            if landmark_coords is None:
-                print(f"Landmarks not extracted for {image_path}")
-                num_undetected_faces += 1
-                continue
-
-            if file_index % 2 == 1:
-                normal.append(landmark_coords)
-            else:
-                altered.append(landmark_coords)
-        except Exception as e:
-            print(f"Error processing {image_path.name}: {e}")
+        if norm_coords is not None and alt_coords is not None:
+            normal.append(norm_coords)
+            altered.append(alt_coords)
+        else:
+            print(f"Skipping Person {person_id} due to detection failure.")
+            num_undetected_faces += 1
 
     face_mesh.close()
     print(num_undetected_faces)
@@ -78,9 +80,9 @@ def generate_dataset_npz(synthetic_dataset_path, output_npz_path):
     normal_batched = np.stack(normal, axis=0) if normal else np.empty((0, 478, 3), dtype=np.float32)
     altered_batched = np.stack(altered, axis=0) if altered else np.empty((0, 478, 3), dtype=np.float32)
 
-    print(f"Normal landmarks shape: {normal_batched.shape}")
-    print(f"Altered landmarks shape: {altered_batched.shape}")
-    print(f"Total undetected faces: {num_undetected_faces}")
+    print(f"Normal landmarks shape: {normal_batched.shape}") # Output: (522, 478, 3)
+    print(f"Altered landmarks shape: {altered_batched.shape}") # Output: (522, 478, 3)
+    print(f"Total undetected faces: {num_undetected_faces}") # Output: 0
 
     np.savez_compressed(
         output_npz_path,
@@ -89,23 +91,72 @@ def generate_dataset_npz(synthetic_dataset_path, output_npz_path):
     )
     print(f"Saved landmarks as .npz to {output_npz_path}")
     
+# construct edge_index attribute for PyTorch graph connectivity
+def graph_connectivity():
+    connections = FaceLandmarksConnections.FACE_LANDMARKS_TESSELATION
+    base_edges = [(c.start, c.end) for c in connections]  # list of tuples representing 468 (iris landmarks not present) MP face landmarker edge connections
 
-def generate_dataset_pt():
-    pass
+    # creating custom edge connections for iris landmarks
+    left_eye_contour = [33, 133, 159, 145, 153, 144, 163, 7]
+    right_eye_contour = [362, 263, 386, 374, 380, 373, 390, 249]  
 
+    custom_edges = []
+    for index in left_eye_contour + [469, 470, 471, 472]:
+        custom_edges.extend([(468, index), (index, 468)])
+    for index in right_eye_contour + [474, 475, 476, 477]:
+        custom_edges.extend([(473, index), (index, 473)])
+
+    all_edges = base_edges + custom_edges
+    bidirectional_edges = set()
+    for u, v in all_edges:
+        bidirectional_edges.add((u, v))
+        bidirectional_edges.add((v, u))
+
+    edge_index = torch.tensor(list(bidirectional_edges), dtype=torch.long).t().contiguous()
+    return edge_index
+
+# Covert the .npz to a PyTorch geometric graph (.pt)
+def generate_dataset_pt(npz_path="/home/alexis/Desktop/landmarks.npz", save_path="/home/alexis/Desktop/landmarks_dataset.pt"):
+    data_npz = np.load(npz_path)
+    normal_coords = data_npz['normal']
+    altered_coords = data_npz['altered']
+
+    edge_index = graph_connectivity()
+
+    # coordinate normalization
+    data_list = []
+    for i in range(len(normal_coords)):
+        norm_raw = torch.tensor(normal_coords[i], dtype=torch.float)
+        alt_raw = torch.tensor(altered_coords[i], dtype=torch.float)
+
+        center = norm_raw[4:5, :]
+
+        scale = torch.norm(norm_raw[468] - norm_raw[473], p=2)
+
+        x_norm = (norm_raw - center)/scale
+        alt_norm = (alt_raw - center)/scale
+
+        y_displacement = alt_norm - x_norm
+
+        graph = Data(
+            x=x_norm,
+            edge_index=edge_index,
+            y=y_displacement,
+            scale=scale,
+            center=center,
+        )
+        data_list.append(graph)
+
+    data, slices = InMemoryDataset.collate(data_list)
+    torch.save((data,slices), save_path)
+
+    print(f"Successfully generated PyTorch Geometric dataset with {len(data_list)} samples as '.pt' to {save_path}.")
+
+    
 synthetic_dataset_path = "/home/alexis/Desktop/synthetic-dataset/ComfyUI/output/cfd_target/"
 output_path = "/home/alexis/Desktop/landmarks.npz"
 
-#generate_dataset_npz(synthetic_dataset_path, output_path)
-
+# generate_dataset_npz(synthetic_dataset_path, output_path)
+generate_dataset_pt()
 # extract_landmarks("/home/alexis/Desktop/synthetic-dataset/ComfyUI/output/cfd_target/result_00563_.png", face_mesh=init_mp())
-
-
-# you should keep all of this logic in this file, and only load the .pt in the gcn.py file. no point in creating a seperate file for it.
-# also note: you should use mediapipe's provided face mesh tesselation (edges/connections between the landmarks) or wireframe.
-# this is because using k-nearest neighbours will build the vertex connections based on pure physical proximity, wheras vertex connections are based on anatomy
-# if mediapipe's tesselation is used:
-# tesselation=vision.FaceLandmarksConnections.FACE_LANDMARKS_TESSELATION
-
-
 
